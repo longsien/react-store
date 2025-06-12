b424dc2e0890730770b89b1d5feb5fb9750cb722
Long Sien <long.sien@monash.edu>
2025-06-12 16:18:43 +1000

    Added minification to babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   presets: [
-    // Compiles modern JavaScript down to ES5
     '@babel/preset-env',
-    // Compiles React features
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
+  env: {
+    minify: {
+      presets: ['babel-preset-minify'],
+    },
+  },
 }
